37e83dacd775b84adcf2348848ca3559e68691e5
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2026-01-18 01:18:34 +0530

    Spring Security, reading User from Database, tested in postman

---
+++ src/main/java/com/example/Spring_OAuth2_app/model/Users.java
@@ -0,0 +1,46 @@
+package com.example.Spring_OAuth2_app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
+}
